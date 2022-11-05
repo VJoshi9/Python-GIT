fc52a2c9cbefca55f9128af46455a44df00043b4
Vinay Kumar Joshi <joshivinay085@gmail.com>
2022-11-05 13:16:54 +0530

    Add files via upload

---
+++ forLoop.py
@@ -0,0 +1,30 @@
+'''cars = ["Tata","BMW","Kia","Nano","Ford"]
+for i in cars:
+    
+    if i == "Kia":
+        break
+    else:
+        print(i)'''
+
+
+'''cars = ["Tata","BMW","Ford"]
+colors = ["red","blue","yellow"]
+for i in cars:
+    for j in colors:
+        print(j,i)'''
+
+num = int(input("Enter a number"))
+a = 0
+for i in range(num+1):
+    a = a+i
+print(a)
+
+
+
+
+
+
+
+
+        
+        
