064cfdcf6244a292d9eee51a71e3f60e4ed98d94
Vinay Kumar Joshi <joshivinay085@gmail.com>
2022-11-19 23:25:19 +0530

    Add files via upload

---
+++ Problems_Strings.py
@@ -0,0 +1,13 @@
+#take a string of length 5[index 0-4]
+#print first char
+#print middle char
+#print last char
+
+string = "Vinay"
+print(string[0])
+print(string[5//2])
+print(string[-1])  
+
+#Home Work: Practice questions on Strings...
+#Committ on github...
+#Start working on HTML dummy project...
